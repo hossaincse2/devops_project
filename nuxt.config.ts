65d39b7c40a369df4dfcc364091f8877475740fc
Mohammad Hossain <hossaincse2@gmail.com>
2024-05-16 23:03:30 +0600

    Revert "Hossain"

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,4 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config 
-
+// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true },
-  modules: ["@nuxtjs/eslint-module"], 
+  devtools: { enabled: true }
 })
